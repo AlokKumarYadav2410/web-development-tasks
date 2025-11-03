a6c3b1cec3fa406e567139030433af6a683e6cd5
SubastralAlok <alokkumaryadav241004@gmail.com>
2025-11-03 20:39:50 +0530

    Added Grade checker

--- Javascript/JsAssignment/Assignment-01/script2.js
+++ Javascript/JsAssignment/Assignment-01/script2.js
@@ -13,3 +13,17 @@ console.log("Script 02 is added using defer")
 const PI = 3.14159;
 const BASE_URL = "https://api.example.com";
 const MAX_USERS = 1000;
+
+//6. Taking marks as input and grade according to the marks.
+let marks = prompt("Enter your marks (0-100):");
+marks = Number(marks);
+function getGrade(marks) {
+    if(marks >= 90 && marks <= 100) return 'A+';
+    else if (marks >= 80) return 'A';
+    else if (marks >= 70) return 'B';
+    else if (marks >= 60) return 'C';
+    else if (marks >= 0) return 'D';
+    else return 'Invalid marks';
+}
+
+console.log(`Your grade is: ${getGrade(marks)}`);
