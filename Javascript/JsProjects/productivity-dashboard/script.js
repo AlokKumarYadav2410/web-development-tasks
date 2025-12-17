45e1d3f9f131b3b508693b0d49479f879e6f7a64
SubastralAlok <alokkumaryadav241004@gmail.com>
2025-12-17 20:36:22 +0530

    Made to do page in productivity project

--- Javascript/JsProjects/productivity-dashboard/script.js
+++ Javascript/JsProjects/productivity-dashboard/script.js
@@ -1,15 +1,19 @@
-let allElems = document.querySelectorAll(".elem");
-let allFullElems = document.querySelectorAll(".fullElem");
-let allFullElemsBackBtns = document.querySelectorAll(".fullElem .back");
+function openFeatures() {
+    let allElems = document.querySelectorAll(".elem");
+    let fullElemPage = document.querySelectorAll(".fullElem");
+    let fullElemPageBackBtns = document.querySelectorAll(".fullElem .back");
 
-allElems.forEach((elem) => {
-    elem.addEventListener("click", () => {
-        allFullElems[elem.id].style.display = "block";
-    });
-})
-
-allFullElemsBackBtns.forEach((back) => {
-    back.addEventListener('click', (e) => {
-        allFullElems[back.id].style.display = "none";
+    allElems.forEach((elem) => {
+        elem.addEventListener("click", () => {
+            fullElemPage[elem.id].style.display = "block";
+        });
     })
-})
+
+    fullElemPageBackBtns.forEach((back) => {
+        back.addEventListener('click', (e) => {
+            fullElemPage[back.id].style.display = "none";
+        })
+    })
+}
+
+openFeatures();
