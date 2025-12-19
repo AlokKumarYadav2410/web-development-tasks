408b37ea1dc4ecbbbc3c8bd5c107b7d4c7e8eb4a
SubastralAlok <alokkumaryadav241004@gmail.com>
2025-12-19 22:50:50 +0530

    Completed todo feature of productivity dashboard

--- Javascript/JsProjects/productivity-dashboard/script.js
+++ Javascript/JsProjects/productivity-dashboard/script.js
@@ -17,3 +17,53 @@ function openFeatures() {
 }
 
 openFeatures();
+
+function todoList() {
+    let form = document.querySelector(".add-task form");
+    let taskInput = document.querySelector(".add-task form input");
+    let taskTextarea = document.querySelector(".add-task form textarea");
+    let taskImpCheckbox = document.querySelector(".add-task form .mark-imp #imp-task");
+
+    let allTasksContainer = document.querySelector(".all-task");
+
+    let currentTask = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    function renderTasks() {
+        localStorage.setItem("tasks", JSON.stringify(currentTask));
+
+        let task = '';
+        currentTask.forEach((t, idx) => {
+            task += `<div class="task">
+                    <div class="task-details">  
+                        <h5>${t.title} <span class="${t.important}">Imp!</span></h5>
+                        <p>${t.details}</p>
+                    </div>
+                    <button id=${idx}>Mark as Completed</button>
+                </div>`;
+        });
+
+        allTasksContainer.innerHTML = task;
+        
+        document.querySelectorAll(".task button").forEach((btn, index) => {
+            btn.addEventListener("click", () => {
+                currentTask.splice(btn.id, 1);
+                renderTasks();
+            });
+        });
+    }
+    renderTasks();
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        currentTask.push({
+            title: taskInput.value,
+            details: taskTextarea.value,
+            important: taskImpCheckbox.checked
+        });
+        renderTasks();
+        taskInput.value = '';
+        taskTextarea.value = '';
+        taskImpCheckbox.checked = false;
+    });
+}
+todoList();
