96e612a9c58f7466dd5478598e2a0acc8ab85065
SubastralAlok <alokkumaryadav241004@gmail.com>
2026-01-08 22:17:06 +0530

    Made Router

--- React/Learning/Day-14/src/App.jsx
+++ React/Learning/Day-14/src/App.jsx
@@ -2,14 +2,25 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Sections from './components/Sections'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const App = () => {
+
+  const routes = createBrowserRouter([
+    {
+      path: "/",
+      element: <Navbar title="This is Navbar Title">
+        <h2>This is Navbar Children</h2>
+      </Navbar>,
+    },
+    {
+      path: '/sections',
+      element: <Sections />
+    }
+  ]);
   return (
     <div className='w-full min-h-screen flex flex-col'>
-      <Navbar title="This is Navbar Title">
-        <h2>This is Navbar Children</h2>
-      </Navbar>
-      <Sections />
+      <RouterProvider router={routes} />
       <Footer ><h2>Made with ❤️ by Alokk</h2></Footer>
     </div>
   )
