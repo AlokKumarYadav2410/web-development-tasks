7d916351372395642ea6f265d2938a04d9928698
SubastralAlok <alokkumaryadav241004@gmail.com>
2026-01-11 23:31:07 +0530

    Added sections

--- React/Learning/Day-15/src/component/Section1.jsx
+++ React/Learning/Day-15/src/component/Section1.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const Section1 = ({ theme }) => {
+  const {id} = useParams();
+  console.log(id)
   return (
     <div>
       <h2>This is the theme that we are getting from parent {theme}</h2>
--- React/Learning/Day-15/src/component/Sections.jsx
+++ React/Learning/Day-15/src/component/Sections.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { useContext } from 'react'
 import { ThemeDataContext } from '../context/ThemeContext';
 import Section1 from './Section1';
+import { Outlet } from 'react-router-dom';
 
 const Sections = () => {
     const value = useContext(ThemeDataContext);
   return (
-    <div className='flex-1 bg-amber-200'>
+    <div className='flex-1 bg-amber-200 mt-14'>
+      <h2>This is {value.theme} theme</h2>
+      <p>Visit our other sections to explore more.</p>
       <Section1 theme={value.theme} />
+      {/* <Outlet /> */}
     </div>
   )
 }
